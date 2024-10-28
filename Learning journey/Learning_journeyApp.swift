d525e80b37cb776b0438dd4757ecb61959b764f4
Nujud98 <ialjod1998@gmail.com>
2024-10-29 00:16:29 +0300

    Initial Commit

---
+++ Learning journey/Learning_journeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_journeyApp.swift
+//  Learning journey
+//
+//  Created by Nujud Alotaibi on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_journeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning journeyTests/Learning_journeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_journeyTests.swift
+//  Learning journeyTests
+//
+//  Created by Nujud Alotaibi on 26/04/1446 AH.
+//
+
+import Testing
+@testable import Learning_journey
+
+struct Learning_journeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
